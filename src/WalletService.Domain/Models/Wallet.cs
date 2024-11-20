9babfc10eece09c58a061f02f82e227642e86af4
Ayisha <ayishetuseidu13@gmail.com>
2024-11-20 23:30:30 +0000

    Add domain model for Wallet

---
+++ src/WalletService.Domain/Models/Wallet.cs
@@ -0,0 +1,53 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using WalletService.Domain.Models.Enums;
+
+namespace WalletService.Domain.Models;
+
+public class Wallet
+{
+    private readonly string _walletName;
+    private readonly WalletType _walletType;
+    private readonly AccountScheme _accountScheme;
+    private readonly string _walletOwner;
+    private readonly string _accountNumber;
+    private readonly DateTime _createdAt;
+    private  bool _isActive;
+    private  DateTime? _updatedAt;
+
+    public Wallet(string name, string accountNumber, WalletType walletType, AccountScheme accountScheme, string owner)
+    {
+        _walletName = name;
+        _walletType = walletType;
+        _accountScheme = accountScheme;
+        _walletOwner = owner;
+        _accountNumber = accountNumber;
+        _createdAt = DateTime.UtcNow;
+        _isActive = true;
+    }
+    [Key]
+    public int Id { get; set; }
+    public string WalletName => _walletName;
+    public string AccountNumber => _accountNumber;
+    public WalletType WalletType => _walletType;
+    public AccountScheme AccountScheme => _accountScheme;
+    public DateTime CreatedAt => _createdAt;
+    public string Owner => _walletOwner;
+    public bool IsActive => _isActive;
+    public DateTime? UpdatedAt => _updatedAt;
+
+
+    public void DeactivateWallet()
+    {
+        _isActive = false;
+        _updatedAt = DateTime.UtcNow;
+    }
+
+    //EF core needs an empty contructor to read data into
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public Wallet()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    {
+        
+    }
+}
---
+++ tests/WalletService.Domain.Tests/Models/WalletTests.cs
@@ -0,0 +1,52 @@
+﻿using WalletService.Domain.Models;
+using WalletService.Domain.Models.Enums;
+using Xunit;
+
+namespace WalletService.Domain.Tests.Models;
+
+public class WalletTests
+{
+    [Fact]
+    public void Wallet_Create_SetsPropertiesCorrectly()
+    {
+        // Arrange
+        const string walletName = "Ayisha's MTN";
+        const string acountNumber = "0246777245";
+        const string walletOwner = "0507157798";
+
+        // Act
+        var result = new Wallet(walletName, acountNumber, WalletType.MOMO, AccountScheme.VODAFONE, walletOwner);
+
+        // Assert
+        Assert.Equal(acountNumber, result.AccountNumber);
+        Assert.Equal(walletName, result.WalletName);
+        Assert.Equal(walletOwner, result.Owner);
+        Assert.Equal(WalletType.MOMO, result.WalletType);
+        Assert.Equal(AccountScheme.VODAFONE, result.AccountScheme);
+        Assert.True(result.IsActive);
+        Assert.Null(result.UpdatedAt);
+    }
+
+    [Fact]
+    public void Wallet_Deactivate_SetsActiveStatusAndUpdatedAtdates()
+    {
+        // Arrange
+        const string walletName = "Ayisha's MTN";
+        const string acountNumber = "0246777245";
+        const string walletOwner = "0507157798";
+
+
+        // Act
+        var result = new Wallet(walletName, acountNumber, WalletType.MOMO, AccountScheme.VODAFONE, walletOwner);
+        result.DeactivateWallet();
+
+        // Assert
+        Assert.Equal(acountNumber, result.AccountNumber);
+        Assert.Equal(walletName, result.WalletName);
+        Assert.Equal(walletOwner, result.Owner);
+        Assert.Equal(WalletType.MOMO, result.WalletType);
+        Assert.Equal(AccountScheme.VODAFONE, result.AccountScheme);
+        Assert.False(result.IsActive);
+        Assert.NotNull(result.UpdatedAt);
+    }
+}
