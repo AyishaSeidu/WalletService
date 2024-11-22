5774cb4083ebe21a9201f3ac837efa9370c50ecf
Ayisha <ayishetuseidu13@gmail.com>
2024-11-22 00:40:09 +0000

    Use file-scope namespaces

--- src/WalletService.Infrastructure/DataContext/WalletServiceContext.cs
+++ src/WalletService.Infrastructure/DataContext/WalletServiceContext.cs
@@ -2,16 +2,15 @@
 using WalletService.Domain.Models;
 using WalletService.Infrastructure.EntityConfigurations;
 
-namespace WalletService.Infrastructure.DataContext
-{
-    public class WalletServiceContext(DbContextOptions<WalletServiceContext> dbContextOptions) : DbContext(dbContextOptions)
-    {
-        public DbSet<Wallet> Wallets { get; set; }
+namespace WalletService.Infrastructure.DataContext;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new WalletEntityConfiguration());
-        }
+public class WalletServiceContext(DbContextOptions<WalletServiceContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Wallet> Wallets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new WalletEntityConfiguration());
     }
 }
--- src/WalletService.Infrastructure/EntityConfigurations/WalletEntityConfiguration.cs
+++ src/WalletService.Infrastructure/EntityConfigurations/WalletEntityConfiguration.cs
@@ -7,48 +7,47 @@ using System.Text;
 using System.Threading.Tasks;
 using WalletService.Domain.Models;
 
-namespace WalletService.Infrastructure.EntityConfigurations
+namespace WalletService.Infrastructure.EntityConfigurations;
+
+internal class WalletEntityConfiguration : IEntityTypeConfiguration<Wallet>
 {
-    internal class WalletEntityConfiguration : IEntityTypeConfiguration<Wallet>
+    public void Configure(EntityTypeBuilder<Wallet> builder)
     {
-        public void Configure(EntityTypeBuilder<Wallet> builder)
-        {
-            builder.ToTable(nameof(Wallet));
+        builder.ToTable(nameof(Wallet));
 
-            builder.Property(p => p.Id)
-                .ValueGeneratedOnAdd();
-            builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+        builder.HasKey(p => p.Id);
 
-            builder.Property(p => p.WalletName)
-                .HasField("_walletName")
-                .IsRequired();
+        builder.Property(p => p.WalletName)
+            .HasField("_walletName")
+            .IsRequired();
 
-            builder.Property(p => p.WalletType)
-                .HasField("_walletType")
-                .IsRequired();
+        builder.Property(p => p.WalletType)
+            .HasField("_walletType")
+            .IsRequired();
 
-            builder.Property(p => p.AccountScheme)
-                .HasField("_accountScheme")
-                .IsRequired();
+        builder.Property(p => p.AccountScheme)
+            .HasField("_accountScheme")
+            .IsRequired();
 
-            builder.Property(p => p.AccountNumber)
-                .HasField("_accountNumber")
-                .IsRequired();
+        builder.Property(p => p.AccountNumber)
+            .HasField("_accountNumber")
+            .IsRequired();
 
-            builder.Property(p => p.Owner)
-                .HasField("_walletOwner")
-                .IsRequired();
+        builder.Property(p => p.Owner)
+            .HasField("_walletOwner")
+            .IsRequired();
 
-            builder.Property(p => p.CreatedAt)
-                .HasField("_createdAt")
-                .IsRequired();
+        builder.Property(p => p.CreatedAt)
+            .HasField("_createdAt")
+            .IsRequired();
 
-            builder.Property(p => p.IsActive)
-                .HasField("_isActive")
-                .IsRequired();
+        builder.Property(p => p.IsActive)
+            .HasField("_isActive")
+            .IsRequired();
 
-            builder.Property(p => p.UpdatedAt)
-                .HasField("_updatedAt");
-        }
+        builder.Property(p => p.UpdatedAt)
+            .HasField("_updatedAt");
     }
 }
