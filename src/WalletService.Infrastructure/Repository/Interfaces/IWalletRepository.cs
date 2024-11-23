881003957f23c500c3abba8663d913cc9da24c43
Ayisha <ayishetuseidu13@gmail.com>
2024-11-23 22:04:19 +0000

    Add repository implementation of adding a new wallet

---
+++ src/WalletService.Infrastructure/Repository/Interfaces/IWalletRepository.cs
@@ -0,0 +1,12 @@
+﻿using WalletService.Domain.Models;
+
+namespace WalletService.Infrastructure.Repository.Interfaces;
+
+public interface IWalletRepository
+{
+    /// <summary>
+    /// Add a wallet to the database
+    /// </summary>
+    /// <returns>An integer representing the added Wallet's Id</returns>
+    Task<int> AddWallet(Wallet wallet);
+}
