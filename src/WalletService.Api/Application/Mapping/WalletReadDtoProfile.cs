a5e517712dd5295d36c1c27c645a4242e617a3e4
Ayisha <ayishetuseidu13@gmail.com>
2024-11-23 07:45:15 +0000

    Configure mapping for wallet read Dto

---
+++ src/WalletService.Api/Application/Mapping/WalletReadDtoProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WalletService.Api.Application.Dtos;
+using WalletService.Domain.Models;
+
+namespace WalletService.Api.Application.Mapping;
+
+public class WalletReadDtoProfile : Profile
+{
+    public WalletReadDtoProfile()
+    {
+        CreateMap<Wallet, WalletReadDto>();
+    }
+}
--- src/WalletService.Api/Program.cs
+++ src/WalletService.Api/Program.cs
@@ -1,6 +1,11 @@
+using WalletService.Api.Application.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddAutoMapper(typeof(WalletReadDtoProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
